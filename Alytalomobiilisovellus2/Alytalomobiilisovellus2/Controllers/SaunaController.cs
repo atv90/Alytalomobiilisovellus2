0fa5b653e71d4c17f3a2d52b5ab485f7dcfedb79
Anne Vaittinen <anne.vaittinen@stud.pointcollege.fi>
2018-05-28 15:29:53 +0300

    Lisätty saunan tila taulukon muodostus

--- Alytalomobiilisovellus2/Alytalomobiilisovellus2/Controllers/SaunaController.cs
+++ Alytalomobiilisovellus2/Alytalomobiilisovellus2/Controllers/SaunaController.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Alytalomobiilisovellus2.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Alytalomobiilisovellus2.Controllers
+namespace MobiiliAlytalo.Views
 {
     public class SaunaController : Controller
     {
@@ -12,6 +14,32 @@ namespace Alytalomobiilisovellus2.Controllers
         public ActionResult Sauna()
         {
             return View();
+        }
+        public JsonResult SaunanTila()
+        {
+            //tietokantahaku
+            AlyTaloEntities entities = new AlyTaloEntities();
+
+            //JavaScript-objektin luominen tietokannasta
+            var stila = (from s in entities.Sauna
+                         select new
+                         {
+                             SaunanTila = s.SaunaTila,
+                             SaunanLampotila = s.SaunaNykyLampotila
+
+                         }).ToList();
+
+            //stila-objekti JSON-muotoon
+            string stilajson = JsonConvert.SerializeObject(stila);
+
+            entities.Dispose(); //muistin vapautus
+
+            //välimuistin hallinta
+            Response.Expires = -1;
+            Response.CacheControl = "no-cache";
+
+            //tulosten palautus
+            return Json(stilajson, JsonRequestBehavior.AllowGet);
         }
     }
 }
