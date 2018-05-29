861c6c5f4125c7e245880bb6bd6da602c2219bb4
Anne Vaittinen <anne.vaittinen@stud.pointcollege.fi>
2018-05-29 15:51:53 +0300

    yritetty lisätä saunan tietojen muokkausikkunaa, ei toimi vielä

--- Alytalomobiilisovellus2/Alytalomobiilisovellus2/Controllers/SaunaController.cs
+++ Alytalomobiilisovellus2/Alytalomobiilisovellus2/Controllers/SaunaController.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,6 +11,7 @@ namespace Alytalomobiilisovellus2.Controllers
 {
     public class SaunaController : Controller
     {
+        private AlyTaloEntities db = new AlyTaloEntities();
         // GET: Sauna
         public ActionResult Sauna()
         {
@@ -41,5 +43,36 @@ namespace Alytalomobiilisovellus2.Controllers
             //tulosten palautus
             return Json(stilajson, JsonRequestBehavior.AllowGet);
         }
+        //GET: tietojen muokkaus
+        public ActionResult MuokkaaSauna(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sauna sauna = db.Sauna.Find(id);
+            if (sauna == null)
+            {
+                return HttpNotFound();
+            }
+            SaunaData s = new SaunaData();
+            s.SaunaTila = sauna.SaunaTila;
+            s.SaunaNykyLampotila = sauna.SaunaNykyLampotila;
+
+            return View(s);
+        }
+        //POST: tietojen muokkaus
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult MuokkaaSauna(SaunaData model)
+        {
+            Sauna s = db.Sauna.Find(model.SaunaTila);
+            s.SaunaTila = model.SaunaTila;
+            s.SaunaNykyLampotila = model.SaunaNykyLampotila;
+
+            db.SaveChanges();
+
+            return RedirectToAction("Sauna");
+        } 
     }
 }
---
+++ Alytalomobiilisovellus2/Alytalomobiilisovellus2/Models/SaunaData.cs
@@ -0,0 +1,12 @@
+﻿namespace Alytalomobiilisovellus2.Models
+{
+    internal class SaunaData
+    {
+        public SaunaData()
+        {
+        }
+
+        public int? SaunaNykyLampotila { get; internal set; }
+        public string SaunaTila { get; internal set; }
+    }
+}
